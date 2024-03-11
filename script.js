0a1dbcf9cbf8e988855c97f2daad5d3ccb4ad75e
mohammed fawzi mohammed <mohammed.mohammed.fawzi@gmail.com>
2024-03-11 12:57:14 +0100

    order general info

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
+let data;
+let jasonFile = 'data.json';
 
 async function fetchData() {
     try {
-        const response = await fetch('data.json');
+        const response = await fetch(jasonFile);
         if (!response.ok) {
-            throw new Error('cant find the data');
+            throw new Error('Cannot find the data');
         }
-        const data = await response.json();
-        console.log(data);
+        data = await response.json();
+        return data;
     } catch (error) {
-        console.error('somthing went wrong withe  JSON data: ', error);
+        console.error('Something went wrong with the JSON data: ', error);
     }
 }
 
-fetchData();
+async function displayJSONData() {
+    try {
+        const theData = await fetchData();
+        const showData = document.getElementById('data_id');
+
+        theData.mvPurchaseOrders.forEach(order => {
+            const orderDiv = document.createElement('div');
+            orderDiv.classList.add('purchase-order');
+
+          
+            const orderInfo = document.createElement('p');
+            orderInfo.textContent = `Purchase Order No: ${order.PurchaseOrderNo}, Status: ${order.PurchaseOrderStatus}`;
+            orderDiv.appendChild(orderInfo);
+
+         
+
+            showData.appendChild(orderDiv);
+        });
+
+        console.log(theData);
+    } catch (error) {
+        console.error('Error displaying JSON data:', error);
+    }
+}
+
+displayJSONData();
