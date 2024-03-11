5c5fb1361e15974f83f1b95f06bc673e8070bbae
mohammed fawzi mohammed <mohammed.mohammed.fawzi@gmail.com>
2024-03-11 11:33:18 +0100

    consol the data and get response

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("hi");
+
+async function fetchData() {
+    try {
+        const response = await fetch('data.json');
+        if (!response.ok) {
+            throw new Error('cant find the data');
+        }
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('somthing went wrong withe  JSON data: ', error);
+    }
+}
+
+fetchData();
